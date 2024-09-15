2b3f3d2a25609b2216f0908e624ffbf3b2f48a00
shubhanshubb <shubhanshubb@gmail.com>
2024-09-16 01:16:53 +0530

    5th Sem Project

---
+++ Common/footer.php
@@ -0,0 +1,45 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>HKBK College of Engineering </h3>
+            <p>This Project has been made by Syed Tajamul Shah and Tarique Ahmad with the help of 
+              our Allah Almighty.
+              The prupose of this project is Learning Computer Languages.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About HKBK College of Engineering</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B. Architecture</a></li>
+              <li><a href="">BS Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: No.22/1, Opposite, Manyata Tech Park Rd, Vyalikaval Society, Vyalikaval HBCS Layout, Nagavara, Bengaluru, Karnataka 560045</p>
+              <p>Telephones: 090350 22250
+</p>
+              <p>E-mail: info@hkbk.edu.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  
+</footer>
